685d2100a95b7bd25ffb91ddf6bdae9112a7d8b8
keisha2705 <moyokeisha8@gmail.com>
2026-09-20 22:36:12 +0200

    change in the url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const { GoogleGenAI } = require("@google/genai");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 const uri = process.env.MONGODB_URI;
 
 
